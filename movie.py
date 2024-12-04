6dc6c692ca54e0cc9b13949d3bc127ed14215b04
krishna-36 <krishna189097@gmail.com>
2024-12-04 20:48:45 +0530

    Delete movie.py

--- movie.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-def extract_audio(video_file):
-    video = VideoFileClip(video_file)
-    audio_file = "extracted_audio.wav"
-    video.audio.write_audiofile(audio_file)
-    return audio_file
